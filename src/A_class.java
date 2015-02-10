67ba1e75f588224f314e1ad96f4ac1000a9a259b
Kristian Madsen <kristian.madsen@sveaekonomi.se>
2015-02-10 15:58:52 +0100

    initial commit, return_class() returns A_class

---
+++ src/A_class.java
@@ -0,0 +1,7 @@
+
+public class A_class {
+
+	public A_class() {
+		System.out.println("new A_class");
+	}
+}
---
+++ src/Builder.java
@@ -0,0 +1,12 @@
+
+public class Builder {
+	boolean b_flag;
+
+	public Builder() {
+		b_flag = false;
+	}
+	
+	public A_class return_class() {
+		return new A_class();
+	}
+}
